f4ec7a8d716114d9f82a0ce779cb76f41a770733
jihong <clara5091@naver.com>
2022-12-29 12:12:07 +0900

    initialize

---
+++ src/main/java/com/example/InstagramCloneCoding/InstagramCloneCodingApplication.java
@@ -0,0 +1,13 @@
+package com.example.InstagramCloneCoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramCloneCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramCloneCodingApplication.class, args);
+	}
+
+}
