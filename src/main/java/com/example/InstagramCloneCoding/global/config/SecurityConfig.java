87a67a983e935ddecc5072e88731b935dc6a9be4
jihong <clara5091@naver.com>
2023-01-31 07:57:52 +0900

    feat: "Security filterChain 기능 추가"
    /login, /register, /confirm-email 접근 빼고 모두 인증 필요하도록 변경
    생성한 JwtAuthenticationFilter를 UsernamePasswordAutehnticationFilter 전에 추가
    생성한 JwtAuthenticationEntryPoint 추가하여 인증된 접근 아닐 시 에러 처리할 수 있도록 함

--- src/main/java/com/example/InstagramCloneCoding/global/config/SecurityConfig.java
+++ src/main/java/com/example/InstagramCloneCoding/global/config/SecurityConfig.java
@@ -1,23 +1,43 @@
 package com.example.InstagramCloneCoding.global.config;
 
+import com.example.InstagramCloneCoding.global.auth.JwtTokenProvider;
+import com.example.InstagramCloneCoding.global.auth.filter.JwtAuthenticationEntryPoint;
+import com.example.InstagramCloneCoding.global.auth.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors().disable()        // cors 방지
-            .csrf().disable()        // csrf 방지
-            .formLogin().disable()   // 기본 로그인 페이지 없애기
-            .headers().frameOptions().disable();
+        http.httpBasic().disable()  // basic auth 사용x
+                .cors().disable()        // cors 방지
+                .csrf().disable()        // csrf 방지
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 사용x(jwt 사용)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/login").permitAll()  // 모든 요청 허가
+                .antMatchers("/register").permitAll()
+                .antMatchers("/confirm-email").permitAll()
+                .anyRequest().authenticated()   // 나머지 인증 필요
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider)
+                        , UsernamePasswordAuthenticationFilter.class)   // JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 전에 넣음
+                .exceptionHandling()
+                .authenticationEntryPoint(new JwtAuthenticationEntryPoint());
 
         return http.build();
     }
