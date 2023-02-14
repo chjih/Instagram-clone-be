221736d735c2284a26da7006889fd0d510487928
Seo Dabin <tjekqls7@naver.com>
2023-02-14 21:28:18 +0900

    feat : "LoggedInUserArgumentResolver 등록"
    컨트롤러 파라미터에 @LoggedInUser Member member 추가하면 security context의 authentication에서 member를 가져와서 자동으로 넣어준다.

--- src/main/java/com/example/InstagramCloneCoding/global/config/WebMvcConfig.java
+++ src/main/java/com/example/InstagramCloneCoding/global/config/WebMvcConfig.java
@@ -1,11 +1,25 @@
 package com.example.InstagramCloneCoding.global.config;
 
+import com.example.InstagramCloneCoding.global.common.resolver.LoggedInUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 @EnableWebMvc
+@RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    @Autowired
+    private final LoggedInUserArgumentResolver loggedInUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loggedInUserArgumentResolver);
+    }
 }
