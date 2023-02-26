a474a68a7159c18b621ebcc6cb0c78e0342359db
jihong <clara5091@naver.com>
2023-02-27 00:10:26 +0900

    feat: "CORS 에러 수정"

--- src/main/java/com/example/InstagramCloneCoding/global/config/WebMvcConfig.java
+++ src/main/java/com/example/InstagramCloneCoding/global/config/WebMvcConfig.java
@@ -25,7 +25,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
                 .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 }
