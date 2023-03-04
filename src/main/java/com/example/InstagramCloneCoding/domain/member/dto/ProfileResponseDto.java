115ffd6fbce018bfd202740984c1e4f49c3d361a
jihong <clara5091@naver.com>
2023-03-05 06:51:10 +0900

    feat: "ProfileResponseDto 생성, MemberResponseDto 변경"
    프로필에 나올 정보들을 전달해주는 ProfileResponseDto 생성, memberResponseDto에는 간단한 정보(아이디, 이름, 프로필사진)만 전달하게 함

---
+++ src/main/java/com/example/InstagramCloneCoding/domain/member/dto/ProfileResponseDto.java
@@ -0,0 +1,38 @@
+package com.example.InstagramCloneCoding.domain.member.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProfileResponseDto {
+    private String userId;
+
+    private String name;
+
+    private String profileImage;
+
+    private String introduction;
+
+    private int followerCount;
+
+    private int followingCount;
+
+    private boolean iFollowed;
+
+    private boolean isMe;
+
+    @Builder
+    public ProfileResponseDto(String userId, String name, String profileImage, String introduction,
+                              int followerCount, int followingCount, boolean iFollowed, boolean isMe) {
+        this.userId = userId;
+        this.name = name;
+        this.profileImage = profileImage;
+        this.introduction = introduction;
+        this.followerCount = followerCount;
+        this.followingCount = followingCount;
+        this.iFollowed = iFollowed;
+        this.isMe = isMe;
+    }
+}
