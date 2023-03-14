f986c19b1195691ce77cbf27b2d2dca3fd4ff3e0
Seo Dabin <tjekqls7@naver.com>
2023-03-14 20:38:30 +0900

    feat : "멤버 삭제 시 댓글 삭제"

--- src/main/java/com/example/InstagramCloneCoding/domain/comment/dao/CommentRepository.java
+++ src/main/java/com/example/InstagramCloneCoding/domain/comment/dao/CommentRepository.java
@@ -2,6 +2,7 @@ package com.example.InstagramCloneCoding.domain.comment.dao;
 
 import com.example.InstagramCloneCoding.domain.comment.domain.Comment;
 import com.example.InstagramCloneCoding.domain.feed.domain.Post;
+import com.example.InstagramCloneCoding.domain.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -15,4 +16,6 @@ public interface CommentRepository extends JpaRepository<Comment, Integer> {
     List<Comment> findByRef(int ref);
 
     List<Comment> findByPostOrderByRefAscRefStepAsc(Post post);
+
+    List<Comment> findByMember(Member member);
 }
